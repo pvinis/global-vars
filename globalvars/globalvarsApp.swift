dc94a117e9b8ca15c4dd828a4901079b31077695
Pavlos Vinieratos <pvinis@gmail.com>
2025-12-25 17:41:47 +0200

    batman

---
+++ globalvars/globalvarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  globalvarsApp.swift
+//  globalvars
+//
+//  Created by Pavlos on 25/12/25.
+//
+
+import SwiftUI
+
+@main
+struct globalvarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
